9172c2d7469696e6d8df77fc4052cd98d08a58b2
Dmitriy <adimas@protonmail.com>
2022-05-07 22:56:24 +0300

    refactoring

--- app/src/main/java/com/example/mapmarkers/MainActivity.kt
+++ app/src/main/java/com/example/mapmarkers/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import androidx.activity.viewModels
 import com.example.mapmarkers.databinding.ActivityMainBinding
 import com.example.mapmarkers.repo.MarkerRepositoryImpl
@@ -48,18 +46,6 @@ class MainActivity : AppCompatActivity() {
             } else {
                 binding.fab.show()
             }
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 
